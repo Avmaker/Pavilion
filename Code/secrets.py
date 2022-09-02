1f5a08d5d75f66ee1580d8069e27b901500cbb19
Avmaker <66441069+Avmaker@users.noreply.github.com>
2022-09-02 14:25:43 +0100

    Update secrets.py

--- Code/secrets.py
+++ Code/secrets.py
@@ -2,15 +2,15 @@
 # If you put them in the code you risk committing that info or sharing it
 
 secrets = {
-    'ssid' : 'VM6353842',
-    'password' : 'fydtygRjg8xy',
-    'openweather_token' : '9f19337de06a0cdea2faa1108443f995',
-    'aio_username' : 'Helenina',
-    'aio_key' : 'dbf3281ddcc14dc4b472beee4889ba0e',
+    'ssid' : 'XXXXXX',
+    'password' : 'xxxxxx',
+    'openweather_token' : 'xxxxxxxxx',
+    'aio_username' : 'xxxxxx',
+    'aio_key' : 'xxxxxxxxxxxx',
     'timezone' : 'Europe/London' ,
     'broker': "io.adafruit.com" ,
     'mqtt_port': 8883,
-    'mqtt_username': 'Helenina',
-    'mqtt_password': 'dbf3281ddcc14dc4b472beee4889ba0e',
+    'mqtt_username': 'xxxxx',
+    'mqtt_password': 'xxxxxxxxxxxx',
     
     }
